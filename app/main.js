96da3a3714548558df6fc0f96e246b8a49d50f4f
João Marcelo Barros Vilas Boas <joaombvb@gmail.com>
2025-03-21 10:33:47 -0300

    init git alura js metodos de arrays

---
+++ app/main.js
@@ -0,0 +1,11 @@
+let livros = [];
+const endPointDaApi = "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+getBuscarLivrosDaApi();
+
+async function getBuscarLivrosDaApi() {
+    const res = await fetch(endPointDaApi);
+    livros = await res.json();
+    let livrosComDesconto = aplicarDesconto(livros);
+
+    exibirLivrosNaTela(livros);
+}
